f1fe7be81a90c8c8323db2a1a7c5897b861609ca
OWO Devs <developers.owo@gmail.com>
2025-04-01 14:11:22 +0200

    chore: rename principal file

---
+++ OWORepo.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWO_REPO
+{
+    [BepInPlugin("org.bepinex.plugins.OWO_REPO", "OWO_REPO", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        #pragma warning disable CS0109
+        internal static new ManualLogSource Log;
+        #pragma warning restore CS0109
+
+        private void Awake()
+        {
+            Log = Logger;
+            Logger.LogMessage("OWO_REPO plugin is loaded!");
+            //owoSkin = new OWOSkin();
+
+            var harmony = new Harmony("owo.patch.repo");
+            harmony.PatchAll();
+        }
+    }
+}
