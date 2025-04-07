669fc036008d42179b2ba917ca719bb27bc828bb
OWO Devs <developers.owo@gmail.com>
2025-04-07 14:11:00 +0200

    chore: made beam loop slower

--- OWOSkin.cs
+++ OWOSkin.cs
@@ -169,7 +169,7 @@ namespace OWO_REPO
             while (beamIsActive)
             {
                 Feel("Grab Beam", 0, beamIntensity);
-                await Task.Delay(100);
+                await Task.Delay(200);
             }
         }
 
